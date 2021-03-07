c3915bc99b80895405988c6573d05054eb0c5676
Paula Nerenberg <paulita.glam@gmail.com>
2021-03-07 18:28:31 +0000

    pretty please!

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,18 @@ import CounterButton from './CounterButton';
 // shallow comparison of current and previous props and state.
 
 class Header extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false
-  }
-  render() {
-    console.log('header')
-    return (
-      <div>
-        <h1 className='f1'>RoboFriends</h1>
-        <CounterButton />
-      </div>
-    );
-  }
+shouldComponentUpdate(nextProps, nextState) {
+return false
+}
+render() {
+console.log('header')
+return (
+<div>
+<h1 className='f1'>RoboFriends</h1>
+<CounterButton />
+</div>
+);
+}
 };
 
 export default Header;
